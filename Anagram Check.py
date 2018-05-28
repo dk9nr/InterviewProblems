06eeed01e7a177a3e3efa662cdfe0d025cb089de
Dev <dev.kumar@thinkingwithtools.com>
2018-05-27 22:05:40 -0400

    added solution

--- Anagram Check.py
+++ Anagram Check.py
@@ -1,3 +1,4 @@
+# White implementation
 def AC (s1,s2):
     d1= {}
     d2 = {}
@@ -14,6 +15,37 @@ def AC (s1,s2):
     for key in d1:
         if key not in d2:
             return False
-        if key.value !== d2.key.value:
+        if key.value != d2.key.value:
             return False
         return True
+
+# First solution
+def anagram(s, t):
+    """
+    :type s: str
+    :type t: str
+    :rtype: bool
+    """
+    s=s.replace(' ','').lower()
+    t=t.replace(' ','').lower()
+    
+    if(len(s)!=len(t)):
+        return False
+    counter = {}
+    for letter in s:
+        if letter in counter:
+            counter[letter] += 1
+        else:
+            counter[letter] = 1
+
+    for letter in t:
+        if letter in counter:
+            counter[letter] -= 1
+        else:
+            return False
+
+    for k in counter:
+        if counter[k]!=0:
+          return False
+    
+    return True
