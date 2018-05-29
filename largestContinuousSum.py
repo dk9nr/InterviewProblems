c852e0d6731f1198ab59250a1e0bc475a20859f4
Dev <dev.kumar@thinkingwithtools.com>
2018-05-29 09:11:41 -0400

    had trouble with this one

--- largestContinuousSum.py
+++ largestContinuousSum.py
@@ -1,23 +1,23 @@
 #whiteboard implementation
 def lcs(arr):
-    min=-999999999
-    sum=0
-    for number in arr:
-        if number != 0:
-            if number < 0 and number > min:
-                min=number
-            elif number > 0:
-                sum +=number
-            else:
-                print("hi")
-    if min==0 and sum==0:
-        print (0)
-    elif sum==0:
-        print (min)
-    else:
-        print (sum)
+    cs=ms=arr[1]
+    for number in arr[1:]:
+        cs=max(cs+number,number)
+        ms=max(cs,ms)
+    print (ms)
+
+#answer
+def largestContinuousSum(arr):
+    if len(arr)==0:
+        return
+    maxSum=currentSum=arr[0]
+    for num in arr[1:]:
+        currentSum=max(currentSum+num, num)
+        maxSum=max(currentSum, maxSum)
+    return maxSum
 
 
 if __name__ == "__main__":
     arr = [1,2,-1,3,4,10,10,-10,-1]
+    arr = [-2,8,9,-2,2]
     lcs(arr)
