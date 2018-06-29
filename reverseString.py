50e1091150cf8173bad009749049a0ae04bda29a
Dev <dev.kumar@thinkingwithtools.com>
2018-06-29 11:51:18 -0400

    classic solution

--- reverseString.py
+++ reverseString.py
@@ -1,6 +1,12 @@
-def reverseString(self, s):
+def reverseString(s):
         s=list(s)
-        ans=[]
-        for letter in s:
-            ans.insert(0,letter)
-        return "".join(ans)
+        i=0
+        j=len(s)-1
+        while i<j:
+            s[i],s[j]=s[j],s[i]
+            i+=1
+            j-=1
+        return "".join(s)
+
+if __name__ == "__main__":
+    print (reverseString("helloThere"))
