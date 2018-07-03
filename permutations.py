7d822537486508e1ccc0b62d0e363a596e6741ee
Dev <dev.kumar@thinkingwithtools.com>
2018-07-03 10:39:23 -0400

    string version

--- permutations.py
+++ permutations.py
@@ -1,16 +1,17 @@
 def perm(s):
     results=[]
+    s=list(s)
     help([],s,results)
     return results
     
 def help(prefix,suffix,results):
     if not suffix:
-        results.append(prefix)
+        results.append("".join(prefix))
     else:
         for i in range(len(suffix)):
                 help(prefix+[suffix[i]],suffix[:i]+suffix[i+1:],results)
 
 
 if __name__ == "__main__":
-    print (perm([1,2,3,4,5]))
+    print (perm('dev'))
 
